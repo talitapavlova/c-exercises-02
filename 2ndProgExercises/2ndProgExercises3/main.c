8af4737241b54162f633928e7d03e1b53248d3cf
talitapavlova <talitapavlova@gmail.com>
2018-04-17 13:31:19 +0200

    not working yet

---
+++ 2ndProgExercises/2ndProgExercises3/main.c
@@ -0,0 +1,20 @@
+/*
+ * 2ndProgExercises3.c
+ *
+ * Created: 4/12/2018 11:12:30 AM
+ * Author : Talita
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+	
+
